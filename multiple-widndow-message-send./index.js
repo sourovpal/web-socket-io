e98d4e3dcb83a8e7f8bd26f3620c48dc93df04e5
Sourov Pal <85163889+sourovpal@users.noreply.github.com>
2023-10-09 11:38:04 +0600

    Create index.js

---
+++ multiple-widndow-message-send./index.js
@@ -0,0 +1,36 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+var browser_clients = {};
+io.on('connection', (socket) => {
+    console.log('connect......')
+
+    
+    socket.on('connect_user', (data)=>{
+        if(browser_clients[data.user_id]){
+            var arr = browser_clients[data.user_id];
+            browser_clients[data.user_id] = [...arr, socket.id];
+        }else{
+            browser_clients[data.user_id] = [socket.id];
+        }
+    });
+
+
+    socket.on('message', (data)=>{
+        // io.to(browser_clients[socket.request._query.user_id]).emit('reply', data)
+        io.to(browser_clients[data.id]).emit('reply', data)
+    });
+
+});
+
+
+
+server.listen(3000, ()=>{
+    console.log('http://localhost:3000/')
+});
